58496fdc7eacba4f1635fa97fd3236da3669d3b0
Burak Pancar <83040277+Burakpncr@users.noreply.github.com>
2021-05-06 23:46:32 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TK,TV>
+    {
+        TK[] dictionaryListKey;
+        TV[] dictionaryListValue;
+
+        public MyDictionary()
+        {
+            dictionaryListKey = new TK[0];
+            dictionaryListValue = new TV[0];
+        }
+        public void Add(TK key,TV value)
+        {
+            TK[] tempListKey = dictionaryListKey;
+            TV[] tempListValue = dictionaryListValue;
+            dictionaryListKey = new TK[dictionaryListKey.Length + 1];
+            dictionaryListValue = new TV[dictionaryListValue.Length + 1];
+            for (int i = 0; i < tempListKey.Length; i++)
+            {
+                dictionaryListKey[i] = tempListKey[i];
+                dictionaryListValue[i] = tempListValue[i];
+            }
+            dictionaryListKey[dictionaryListKey.Length - 1] = key;
+            dictionaryListValue[dictionaryListValue.Length - 1] = value;
+        }
+       
+        public int Count 
+        {
+            get { return dictionaryListKey.Length; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(1, "Burak");
+            sehirler.Add(1, "Burak");
+            sehirler.Add(1, "Burak");
+            Console.WriteLine(sehirler.Count);
+            
+        }
+    }
+}
